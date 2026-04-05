b239c7af3cb1855fcb6d1f0549bfd02a584a5940
Jorge Miranda <jmiranda32691@gmail.com>
2026-04-05 16:24:23 -0500

    restructured client folder

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -9,3 +9,22 @@ export async function createUser(email, password) {
 
     return user;
 }
+
+export async function getUserById(id){
+    const text = 'select * from users where id=$1';
+
+    const { rows: [user]} = await db.query(text, [id]);
+    return user;
+}
+
+export async function getUser(email,password){
+    const text = 'select * from users where email = $1';
+
+    const { rows:[user] } = await db.query(text,[email]);
+    if(!user) return null;
+
+    const isValid = await bcrypt.compare(password, user.password_hash);
+    if (!isValid) return null;
+
+    return user;
+}
